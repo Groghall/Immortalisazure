c0b2b05b9c22fcd38ab641d0f280f45c5288ea5f
Steve Gault <stephengault626@googlemail.com>
2019-06-05 18:45:28 +0100

    site js twaek

--- js/site.js
+++ js/site.js
@@ -1,7 +1,13 @@
-var images = document.getElementsByTagName("img");
-for(var i = 0; i < images.length; i++) {
-    var image = images[i];
-    image.onclick = function(event) {
-         window.location.href = this.id + '.html';
-    };
-}
+// var images = document.getElementsByTagName("img");
+// for(var i = 0; i < images.length; i++) {
+//     var image = images[i];
+//     image.onclick = function(event) {
+//          window.location.href = this.id + '.html';
+//     };
+// }
+
+
+var returnLocation = document.getElementById("index");
+returnLocation.onclick = function(e) {
+    window.location.href = this.id + '.html';
+};
